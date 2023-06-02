0d472fc8806505618a80b71107535320859c4451
Dmitry Gonokhov <gonokhov.dm@gmail.com>
2023-06-02 11:30:16 +0700

    implemented search action & style search item

---
+++ src/components/SearchResult/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './SearchResult.scss'
+
+import userSvg from '../../svg/photo.svg';
+
+function SearchResult( { user } ) {
+  return (
+    <div className="search-result">
+      {typeof user !== "string" ? 
+        (user.map((item, index) => (
+          <div className="search-result__item" key={index}>
+            <img src={userSvg} alt="user" />
+            <div className="search-result__item-info">
+              <div>
+                <h3>{item.username}</h3>
+                <p>{item.email}</p> 
+              </div>
+            </div>
+          </div>
+        ))) : <p>{user}</p>
+      }
+    </div>
+  )
+}
+
+export default SearchResult
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,36 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+
+import SearchResult from '../SearchResult';
 
 import './Sidebar.scss';
 
 function Sidebar() {
 
-//const [searchValue, setSearchValue] = useState('');
+const [searchValue, setSearchValue] = useState('');
 const [user, setUser] = useState('');    
 
 async function logJSONData(value) {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const jsonData = await response.json();
+  let counter;
+  let searchArr = [];
+  counter = 0;
   for (let i = 0; i < jsonData.length; i++) {
-    if ((jsonData[i].username === value) || (jsonData[i].id === Number(value))) {
-      setUser(jsonData[i]);
+    for (let j = 0; j < value.length; j++) {
+      if ((jsonData[i].username === value[j]) || (jsonData[i].id === Number(value[j]))) {
+        searchArr[searchArr.length] = jsonData[i];
+        setUser(searchArr);
+        counter++;
+      }
     }
+  }
+
+  if (counter === 0) {
+    setUser("Ничего не найдено")
   }
 }
 
   const onChangeSearch = (event) => {
-    //setSearchValue(event.target.value);
-    logJSONData(event.target.value);
+    setSearchValue(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    let splitSearch = searchValue.split(',');
+    logJSONData(splitSearch);
   }
 
   return (
     <div className="sidebar">
-      <form action="POST" className="sidebar__search">
+      <form action="POST" className="sidebar__search" onSubmit={onSubmit}>
         <h2>Поиск сотрудников</h2>
-        <input type="text" onChange={onChangeSearch} placeholder='Введите id или имя'/>
+        <input type="text" placeholder='Введите id или имя' onChange={onChangeSearch}/>
       </form>
       <div className="sidebar__results">
         <h2>Результаты</h2>
-        {user.name}
+       { user && <SearchResult user={user}/>}
       </div>
     </div>
   )
